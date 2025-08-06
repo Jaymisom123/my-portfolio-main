5eefc6d3f03c5c3aac9a692bb2c787372d6b1756
Jaymisom123 <jaymerego2015@gmail.com>
2025-08-06 17:19:16 -0300

    melhorias

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c240634e7190af6"; }
+/******/ 		__webpack_require__.h = function() { return "a1f2c38f3db0ef21"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
